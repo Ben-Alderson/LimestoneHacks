65342f9e5e72161dbaa832b588a2d8ba481b9baa
Ben-Alderson <bennyalderson@gmail.com>
2016-12-03 12:09:48 -0500

    Add citizens and canvas

--- main.js
+++ main.js
@@ -1,9 +1,33 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext('2d');
 
-function draw() {
-  ctx.fillStyle = "rgb(200,0,0)"; // sets the color to fill in the rectangle with
-ctx.fillRect(10, 10, 55, 50);   // draws the rectangle at position 10, 10 with a width of 55 and a height of 50
+function Citizen() {
+  this.team = "neutral";
+  this.x;
+  this.y;
+
+  this.draw = function() {
+    console.log(this);
+    switch(this.team) {
+    case "neutral":
+      ctx.fillStyle = "#ffffff"
+      ctx.strokeStyle = "#000000"
+      break;
+    }
+
+    ctx.fillRect(10 + Math.random()*10, 10, 55, 50);
+    ctx.strokeRect(10 + Math.random()*10, 10, 55, 50);
+  }
 }
 
-draw();
+var citizens = [];
+citizens.push(new Citizen());
+
+function draw() {
+  for(var c of citizens) {
+    c.draw();
+  }
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+setInterval(draw, 16);
