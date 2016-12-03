c2a2af7f25a621d2fb28ab6d03680545dd0e514b
Laptop <frenchtallama@hotmail.com>
2016-12-03 12:22:47 -0500

    Added racism

--- citizen.js
+++ citizen.js
@@ -6,12 +6,17 @@ function Citizen() {
   this.draw = function() {
     console.log(this)
     switch(this.team) {
+	case "red":
+      ctx.fillStyle = "#ff0000"
+      ctx.strokeStyle = "#000000"
+	case "blue":
+      ctx.fillStyle = "#00ffff"
+      ctx.strokeStyle = "#000000"
     case "neutral":
       ctx.fillStyle = "#ffffff"
       ctx.strokeStyle = "#000000"
       break
     }
-
     ctx.fillRect(10 + Math.random()*10, 10 + Math.random()*10, 55, 50)
     ctx.strokeRect(10 + Math.random()*10, 10 + Math.random()*10, 55, 50)
   }
