6125c2acd7bd5afc10046d30ec3bd64791bb8f74
Ben-Alderson <bennyalderson@gmail.com>
2016-12-03 12:10:44 -0500

    Remove all semicolons

--- main.js
+++ main.js
@@ -1,33 +1,33 @@
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext('2d');
+var canvas = document.getElementById("canvas")
+var ctx = canvas.getContext('2d')
 
 function Citizen() {
-  this.team = "neutral";
-  this.x;
-  this.y;
+  this.team = "neutral"
+  this.x
+  this.y
 
   this.draw = function() {
-    console.log(this);
+    console.log(this)
     switch(this.team) {
     case "neutral":
       ctx.fillStyle = "#ffffff"
       ctx.strokeStyle = "#000000"
-      break;
+      break
     }
 
-    ctx.fillRect(10 + Math.random()*10, 10, 55, 50);
-    ctx.strokeRect(10 + Math.random()*10, 10, 55, 50);
+    ctx.fillRect(10 + Math.random()*10, 10, 55, 50)
+    ctx.strokeRect(10 + Math.random()*10, 10, 55, 50)
   }
 }
 
 var citizens = [];
-citizens.push(new Citizen());
+citizens.push(new Citizen())
 
 function draw() {
   for(var c of citizens) {
-    c.draw();
+    c.draw()
   }
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
 }
 
-setInterval(draw, 16);
+setInterval(draw, 16)
